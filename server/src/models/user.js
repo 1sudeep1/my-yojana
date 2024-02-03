4ec69486b9846e1e168f6f8692da2195c8971290
Sudeep Rajbanshi <isudp.th@gmail.com>
2024-02-03 18:16:28 +0545

    model for user is created

---
+++ server/src/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+//define the shape of the user documents in the collection
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    fullName: String, //String is shorthand of type:String
+    password: String,
+    Organization: String,
+    role: { 
+        type: String, 
+        enum:['Project Manager', 'Developer', 'Designer', 'Staff', 'Software Engineer'],
+        default:'Staff'
+    }
+})
+
+const User= mongoose.model('User', userSchema)
+
+module.exports= User
