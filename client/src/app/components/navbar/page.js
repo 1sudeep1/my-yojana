b4b37b89cd57718fedca99204e2758417fdab711
Sudeep Tharu <sudeep.tharu32@gmail.com>
2024-02-18 18:59:58 +0545

    static profile dropdown added

--- client/src/app/components/navbar/page.js
+++ client/src/app/components/navbar/page.js
@@ -1,14 +1,15 @@
 "use client";
 import React from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar } from "@nextui-org/react";
 import Link from "next/link";
 import Image from "next/image";
 const page = () => {
+    
     return (
         <>
             <Navbar>
                 <NavbarBrand as={Link} href='/'>
-                <Image src="/yojana-logo.png" width={110} height={110} />
+                    <Image src="/yojana-logo.png" width={110} height={110} />
                 </NavbarBrand>
                 <NavbarContent className="hidden sm:flex gap-4" justify="center">
                     <NavbarItem>
@@ -28,14 +29,43 @@ const page = () => {
                     </NavbarItem>
                 </NavbarContent>
                 <NavbarContent justify="end">
-                <Button as={Link} color="primary" href="/login" variant="flat">
-                            Login
-                        </Button>
+                    <Button as={Link} color="primary" href="/login" variant="flat">
+                        Login
+                    </Button>
                     <NavbarItem>
                         <Button as={Link} color="primary" href="/register" variant="flat">
                             Sign Up
                         </Button>
                     </NavbarItem>
+
+                    <Dropdown placement="bottom-end">
+                        <DropdownTrigger>
+                            <Avatar
+                                isBordered
+                                as="button"
+                                className="transition-transform"
+                                color="secondary"
+                                name="Jason Hughes"
+                                size="sm"
+                                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                            />
+                        </DropdownTrigger>
+                        <DropdownMenu aria-label="Profile Actions" variant="flat">
+                            <DropdownItem key="profile" className="h-14 gap-2">
+                                <p className="font-semibold">Signed in as</p>
+                                <p className="font-semibold">zoey@example.com</p>
+                            </DropdownItem>
+                            <DropdownItem key="settings">My Settings</DropdownItem>
+                            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+                            <DropdownItem key="analytics">Analytics</DropdownItem>
+                            <DropdownItem key="system">System</DropdownItem>
+                            <DropdownItem key="configurations">Configurations</DropdownItem>
+                            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+                            <DropdownItem key="logout" color="danger">
+                                Log Out
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
                 </NavbarContent>
             </Navbar>
         </>
