08a674b8e1b62d02d942b7fbbc77ac9131f5ee36
Sudeep Tharu <sudeep.tharu32@gmail.com>
2024-02-20 21:48:07 +0545

    redux logger setup done

--- client/src/app/redux/store.js
+++ client/src/app/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import userReducer from './reducerSlices/userSlice'
 import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
+import logger from 'redux-logger'
 const persistConfig = {
     key: 'root',
     storage,
@@ -12,6 +13,7 @@ const reducer = combineReducers({
 const persistedReducer = persistReducer(persistConfig, reducer)
 export const store = configureStore({
     reducer: persistedReducer,
+    middleware: ()=>[logger]
 })
 
 export const persistor = persistStore(store)
