39a74f1887c6d066bd3eb0a64c733500835cdb52
Sudeep Tharu <sudeep.tharu32@gmail.com>
2024-04-09 13:02:28 +0545

    card and modal component is created and called in project page

---
+++ client/src/app/components/cardView/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+const CardView = () => {
+  return (
+    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+        <Card shadow="sm" isPressable onPress={() => console.log("item pressed")}>
+          <CardBody className="overflow-visible p-0">
+          </CardBody>
+          <CardFooter className="text-small justify-between">
+            <b>Hello</b>
+            <p className="text-default-500">211</p>
+          </CardFooter>
+        </Card>
+    </div>
+  )
+}
+
+export default CardView
--- client/src/app/projects/page.js
+++ client/src/app/projects/page.js
@@ -1,9 +1,17 @@
+'use client'
 import React from 'react'
-
+import CardView from '../components/cardView/page'
+import {Button, useDisclosure} from "@nextui-org/react"
+import ModalView from '../components/modalView/page'
 const Projects = () => {
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
   return (
     <div>
-      Projects
+      <Button onPress={onOpen} className='m-4' color='primary'>Add Projects</Button>
+      <ModalView title="Add Projects" isOpen={isOpen} onOpen={onOpen} onOpenChange={onOpenChange}>
+        hello this is a form
+      </ModalView>
+      <CardView />
     </div>
   )
 }
