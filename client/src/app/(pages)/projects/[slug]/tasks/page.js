2f271ba56089c8c1ad6dfcf754f7b2eb6ff7ce6c
Sudeep Tharu <sudeep.tharu32@gmail.com>
2024-05-08 21:36:04 +0545

    sortable done with react shortable js

--- client/src/app/(pages)/projects/[slug]/tasks/page.js
+++ client/src/app/(pages)/projects/[slug]/tasks/page.js
@@ -1,37 +1,8 @@
 "use client"
 import AdminLayout from '@/app/components/adminLayout/page'
-import { Button, Input } from '@nextui-org/react'
-import React, { useEffect, useRef, useState } from 'react'
+import { Button} from '@nextui-org/react'
+import React, {useState } from 'react'
 import { ReactSortable } from "react-sortablejs";
-
-const Tasks = () => {
-  const inputField = useRef(null);
-  const [sprintList, setSprintList] = useState([])
-  const [issueList, setIssueList] = useState([])
-  const [activeForm, setActiveForm] = useState([])
-
-  const handleSprint = () => {
-    const existingSprintList = [...sprintList]
-    const sprintDetails = { sprintName: 'Yojana ' + (sprintList.length + 1) }
-    existingSprintList.push(sprintDetails)
-    setSprintList(existingSprintList)
-  }
-
-  const handleActiveForm = (index) => {
-    setActiveForm(index)
-  }
-
-  useEffect(() => {
-    document.body.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
-        setIssueList(prevState => {
-          if (!prevState.includes(inputField.current.value)) {
-            return ([...prevState, { id: prevState.length + 1, issueName: inputField.current.value }])
-          } else return [...prevState]
-        })
-      }
-    })
-  }, [])
 
   const sortableOptions = {
     animation: 150,
@@ -41,92 +12,19 @@ const Tasks = () => {
     group: "shared",
     forceFallback: true
   };
-  const SudeepSort = () => {
-    const [blocks, setBlocks] = useState([
-      {
-        id: 1,
-        content: "item 1",
-        parent_id: null,
-        type: "container",
-        children: [
-          {
-            id: 2,
-            content: "item 2",
-            width: 3,
-            type: "text",
-            parent_id: 1
-          },
-          {
-            id: 3,
-            content: "item 3",
-            width: 3,
-            type: "text",
-            parent_id: 1
-          }
-        ]
-      },
-      {
-        id: 4,
-        content: "item 2",
-        parent_id: null,
-        type: "container",
-        children: [
-          {
-            id: 5,
-            content: "item 5",
-            width: 3,
-            parent_id: 2,
-            type: "container",
-            children: [
-              { id: 8, content: "item 8", width: 6, type: "text", parent_id: 5 },
-              { id: 9, content: "item 9", width: 6, type: "text", parent_id: 5 }
-            ]
-          },
-          {
-            id: 6,
-            content: "item 6",
-            width: 2,
-            type: "text",
-            parent_id: 2
-          },
-          {
-            id: 7,
-            content: "item 7",
-            width: 2,
-            type: "text",
-            parent_id: 2
-          }
-        ]
-      }
-    ]);
 
-    return (
-      <div>
-        <ReactSortable list={blocks} setList={setBlocks} {...sortableOptions}>
-          {blocks.map((block, blockIndex) => (
-            <BlockWrapper
-              key={block.id}
-              block={block}
-              blockIndex={[blockIndex]}
-              setBlocks={setBlocks}
-            />
-          ))}
-        </ReactSortable>
-      </div>
-    );
-  }
-  function Container({ block, blockIndex, setBlocks }) {
+  function Container({ sprintListItem, sprintIndex, setSprintLists }) {
     return (
       <>
         <ReactSortable
-          key={block.id}
-          list={block.children}
+          key={sprintListItem.id}
+          list={sprintListItem.tasks}
           setList={(currentList) => {
-            setBlocks((sourceList) => {
+            setSprintLists((sourceList) => {
               const tempList = [...sourceList];
-              const _blockIndex = [...blockIndex];
-              const lastIndex = _blockIndex.pop();
-              const lastArr = _blockIndex.reduce(
+              const _sprintIndex = [...sprintIndex];
+              const lastIndex = _sprintIndex.pop();
+              const lastArr = _sprintIndex.reduce(
                 (arr, i) => arr[i]["children"],
                 tempList
               );
@@ -137,14 +35,14 @@ const Tasks = () => {
           }}
           {...sortableOptions}
         >
-          {block.children &&
-            block.children.map((childBlock, index) => {
+          {sprintListItem.children &&
+            sprintListItem.children.map((taskItem, taskId) => {
               return (
                 <BlockWrapper
-                  key={childBlock.id}
-                  block={childBlock}
-                  blockIndex={[...blockIndex, index]}
-                  setBlocks={setBlocks}
+                  key={taskId}
+                  sprintListItem={taskItem}
+                  sprintIndex={[...sprintIndex, taskId]}
+                  setSprintLists={setSprintLists}
                 />
               );
             })}
@@ -152,40 +50,95 @@ const Tasks = () => {
       </>
     );
   }
-  function BlockWrapper({ block, blockIndex, setBlocks }) {
-    // console.log(block);
-    if (!block) return null;
-    if (block.type === "container") {
+  function BlockWrapper({ sprintListItem, sprintIndex, setSprintLists }) {
+    if (!sprintListItem) return null;
+    if (sprintListItem.type === "container") {
       return (
-        <div className="block">
-          container: {block.content}
+        <div className="flex flex-col gap-y-3 bg-red-500 p-4 my-3">
+          Yojana: {sprintListItem.sprintTitle}
           <Container
-            block={block}
-            setBlocks={setBlocks}
-            blockIndex={blockIndex}
+            sprintListItem={sprintListItem}
+            setSprintLists={setSprintLists}
+            sprintIndex={sprintIndex}
           />
         </div>
       );
     } else {
       return (
-        <div className="block">
-          text: {block.content}
+        <div className="block bg-green-600 ms-4 p-2">
+          {sprintListItem.taskTitle}
         </div>
       );
     }
   }
+  const Tasks = () => {
+    const [sprintLists, setSprintLists] = useState([
+      {
+        id: 1,
+        sprintTitle: "1",
+        parent_id: null,
+        type: "container",
+        tasks: [
+          {
+            id: 1,
+            taskTitle: "Register Page",
+            width: 3,
+            type: "text",
+            parent_id: 1
+          },
+          {
+            id: 2,
+            taskTitle: "Login Page",
+            width: 3,
+            type: "text",
+            parent_id: 1
+          }
+        ]
+      },
+      {
+        id: 2,
+        sprintTitle: "2",
+        parent_id: null,
+        type: "container",
+        tasks: [
+          {
+            id: 3,
+            taskTitle: "Change Password",
+            width: 3,
+            type: "text",
+            parent_id: 2
+          },
+          {
+            id: 4,
+            taskTitle: "Auth Login",
+            width: 3,
+            type: "text",
+            parent_id: 2
+          }
+        ]
+      },
+    ]);
 
-  return (
-    <>
-      <AdminLayout>
-        {JSON.stringify(issueList)}
+    return (
+      <div className='w-full'>
+        <AdminLayout>
         <div className='flex flex-col items-end gap-5 p-5 h-full'>
-          <Button onClick={handleSprint}>Create sprint</Button>
-          <SudeepSort />
+          <Button>Create sprint</Button>
         </div>
-      </AdminLayout>
-    </>
-  )
-}
+        <ReactSortable list={sprintLists} setList={setSprintLists} {...sortableOptions}>
+          {sprintLists.map((sprintListItem, sprintListId) => (
+            <BlockWrapper
+            sprintListId={sprintListId}
+            sprintListItem={sprintListItem}
+              sprintIndex={[sprintListId]}
+              setSprintLists={setSprintLists}
+            />
+          ))}
+        </ReactSortable>
+        </AdminLayout>
+      </div>
+    );
+  }
+ 
 
 export default Tasks
